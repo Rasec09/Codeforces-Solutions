d00589a40d0f23d51c1245c4cab3d982f091c288
Rasec09 <malcom_hades@hotmail.com>
2020-07-21 03:53:52 -0500

    Create A.cpp

---
+++ Rounds/1372/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+
+int main() {
+	optimizar_io
+	int tc, n;
+
+	cin >> tc;
+	while (tc--) {
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cout << "1 ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
