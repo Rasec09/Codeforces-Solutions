0b43e8f3bd4d361d0f7fd20956d2c72673d8dcf4
Rasec09 <malcom_hades@hotmail.com>
2021-01-09 21:17:23 -0600

    Create B.cpp

---
+++ Rounds/1438/B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+
+typedef long long int ll;
+
+int main() {
+	optimizar_io
+	int tc, n, a[200010];
+
+	cin >> tc;
+	while (tc--) {
+		cin >> n;
+		for (int i = 0; i < n; i++) cin >> a[i];
+		map<int,int> freq;
+		for (int i = 0; i < n; i++)
+			freq[a[i]++];
+		cout << (freq.size() != n ? "YES\n" : "NO\n");
+	}
+	return 0;
+}
