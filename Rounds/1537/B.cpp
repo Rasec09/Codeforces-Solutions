405b3c765c0409ae05fbedcf2060f01d8b4d0c15
Rasec09 <malcom_hades@hotmail.com>
2021-06-19 04:27:58 -0500

    Create B.cpp

---
+++ Rounds/1537/B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+
+int main() {
+	optimizar_io
+	int tc, n, m, x, y;
+
+	cin >> tc;
+	while (tc--) {
+		cin >> n >> m >> x >> y;
+		cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+	}
+	return 0;
+}
