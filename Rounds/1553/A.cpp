d300812008a8815c5be6923ec385c8902b6cbd2d
Rasec09 <malcom_hades@hotmail.com>
2021-07-24 22:09:26 -0500

    Create A.cpp

---
+++ Rounds/1553/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+
+int main() {
+	optimizar_io
+	int tc, n;
+
+	cin >> tc;
+	while (tc--) {
+		cin >> n;
+		n++;
+		cout << n / 10 << "\n";
+	}
+	return 0;
+}
+/* stuff you should look for:
+	* int overflow, array bounds
+	* special cases (n=1?), even odd
+	* DON'T GET STUCK ON ONE APPROACH
+*/
