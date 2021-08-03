e9ce90720536508c00d14c38f94d33406b1fdd31
Rasec09 <malcom_hades@hotmail.com>
2021-08-02 19:35:48 -0500

    Create D.cpp

---
+++ Rounds/1554/D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+
+int main() {
+	optimizar_io
+	int tc, n;
+
+	cin >> tc;
+	while (tc--) {
+		cin >> n;
+		string s(n, 'a');
+		if (n & 1) {
+			s[n / 2] = 'b';
+			s[n - 1] = 'c';
+		}
+		else {
+			s[n / 2] = 'b';
+		}
+		cout << s << "\n";
+	}
+	return 0;
+}
+/* stuff you should look for:
+	* int overflow, array bounds
+	* special cases (n=1?), even odd
+	* DON'T GET STUCK ON ONE APPROACH
+*/
