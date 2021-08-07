bb8ff0a9e9d9c911c709ccac792cdc39e89e4ca4
Rasec09 <malcom_hades@hotmail.com>
2021-08-07 15:57:44 -0500

    Create A.cpp

---
+++ Rounds/1549/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimizar_io ios_base::sync_with_stdio(false);cin.tie(0);
+ 
+typedef long long int ll;
+ 
+int main() {
+	optimizar_io
+	int tc, p;
+
+	cin >> tc;
+	while (tc--) {
+		cin >> p;
+		cout << 2 << " " << p - 1 << "\n";
+	}
+	return 0;
+}
+/* stuff you should look for:
+	* int overflow, array bounds
+	* special cases (n=1?), even odd
+	* DON'T GET STUCK ON ONE APPROACH
+*/
